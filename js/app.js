7f0cbd4523292555b259e7c321210d97d9aa2585
Alicoder001 <coderkuchkarov@gmail.com>
2022-12-12 04:11:27 +0500

    added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const togle_btn = document.querySelector(".toggle-btn");
 const header_block = document.querySelector(".header__block");
+const strelbottom = document.querySelector(".strelbottom");
+const subt = document.querySelector(".faq__item-subt");
 
 togle_btn.addEventListener("click", () => {
-	header_block.classList.toggle("hidden");
-	header_block.classList.toggle("smooth");
-	
+	header_block.classList.toggleAll("hidden");
+	header_block.classList.toggleAll("smooth");
+});
+strelbottom.addEventListener("click", () => {
+	subt.classList.toggle("subt-hidden");
 });
